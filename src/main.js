20404ee37c3bda8fa9f90a63080101adcad93d0b
r_j <9616070+r-j@user.noreply.gitee.com>
2021-08-28 15:44:43 +0000

    update src/main.js.

--- src/main.js
+++ src/main.js
@@ -5,12 +5,21 @@ import "./assets/css/base.less";
 import "./assets/css/common.less";
 import "./mock";
 
-import { Button, Upload, Input } from "element-ui";
+import { Message, Button, Upload, Input, Form, FormItem } from "element-ui";
+
+// 导入axios包
+import axios from "axios";
+// 配置请求根路径
+axios.defaults.baseURL = "http://127.0.0.1:8888/api/private/v1/";
+// 将axios包挂载到vue原型对象上
+Vue.prototype.$http = axios;
 
 Vue.use(Button);
 Vue.use(Upload);
 Vue.use(Input);
-
+Vue.use(Form);
+Vue.use(FormItem);
+Vue.prototype.$message = Message;
 Vue.config.productionTip = false;
 
 new Vue({
